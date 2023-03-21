9a57f86fe3d78aa86dcd781f4576f4f3f7d0801f
Hond-aitai01 <mikeonditi333@gmail.com>
2023-03-21 12:25:01 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,27 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * return: Always 0
+ * main - Entry point, prints _putchar
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-    _putchar('H');
-    _putchar('e');
-    _putchar('l');
-    _putchar('l');
-    _putchar('o');
-    _putchar(',');
-    _putchar(' ');
-    _putchar('w');
-    _putchar('o');
-    _putchar('r');
-    _putchar('l');
-    _putchar('d');
-    _putchar('!');
-    _putchar('\n');
-    return (0);
+
+_putchar('_');
+_putchar('H');
+_putchar('e');
+_putchar('l');
+_putchar('l');
+_putchar('o');
+_putchar(',');
+_putchar(' ');
+_putchar('w');
+_putchar('o');
+_putchar('r');
+_putchar('l');
+_putchar('d');
+_putchar('!');
+_putchar('\n');
+return (0);
 }
