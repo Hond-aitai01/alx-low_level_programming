19a8aab49f6089af7f6396e749cfffbc23539384
Hond-aitai01 <mikeonditi333@gmail.com>
2023-03-21 12:31:53 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,19 +9,13 @@ int main(void)
 {
 
 _putchar('_');
-_putchar('H');
-_putchar('e');
-_putchar('l');
-_putchar('l');
-_putchar('o');
-_putchar(',');
-_putchar(' ');
-_putchar('w');
-_putchar('o');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
 _putchar('r');
-_putchar('l');
-_putchar('d');
-_putchar('!');
 _putchar('\n');
 return (0);
 }
